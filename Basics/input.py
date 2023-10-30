c70d64880b930909cecc4e31c063b8739c039deb
YashpalRathod14 <135158197+YashpalRathod14@users.noreply.github.com>
2023-10-30 17:40:25 +0530

    Create input.py

---
+++ Basics/input.py
@@ -0,0 +1,14 @@
+a = input("enter a number")
+print(a + 10)
+
+''' above is error pf diff. data types '''
+
+a = input("enter a string")        # default input is taken as string
+print(a + '10')
+
+''' above is treated like string '''
+
+a = int(input("enter a number"))
+print(a + 10)
+
+''' converted to int data type (typecasting) '''
